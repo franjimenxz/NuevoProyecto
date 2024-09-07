75547ebfd134bee423406d7ca9554e21b459e201
Franco Jimenez <francojimenez31@gmail.com>
2024-09-07 10:37:43 -0300

    fixuser

--- routes/dashboard.py
+++ routes/dashboard.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template
 from models import Ingreso, Egreso
-from flask_login import login_required
+from flask_login import login_required, current_user
 from extensions import db
 
 
@@ -9,22 +9,21 @@ dashboard_bp = Blueprint('dashboard', __name__)
 @dashboard_bp.route('/')
 @login_required
 def index():
-    # Datos totales
-    ingresos = sum(i.importe for i in Ingreso.query.all())
-    egresos = sum(e.importe for e in Egreso.query.all())
+    # Filtrar ingresos y egresos por el usuario actual
+    ingresos = sum(i.importe for i in Ingreso.query.filter_by(legajousuario=current_user.legajo).all())
+    egresos = sum(e.importe for e in Egreso.query.filter_by(legajousuario=current_user.legajo).all())
     
-    # Datos por categoría (ingresos)
+    # Filtrar datos por categoría para el usuario actual
     ingresos_por_categoria = {}
-    for ingreso in Ingreso.query.all():
+    for ingreso in Ingreso.query.filter_by(legajousuario=current_user.legajo).all():
         categoria = ingreso.categoria.nombre
         if categoria in ingresos_por_categoria:
             ingresos_por_categoria[categoria] += ingreso.importe
         else:
             ingresos_por_categoria[categoria] = ingreso.importe
     
-    # Datos por categoría (egresos)
     egresos_por_categoria = {}
-    for egreso in Egreso.query.all():
+    for egreso in Egreso.query.filter_by(legajousuario=current_user.legajo).all():
         categoria = egreso.categoria.nombre
         if categoria in egresos_por_categoria:
             egresos_por_categoria[categoria] += egreso.importe
